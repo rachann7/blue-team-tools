7d85db360ff9fca42911fee02ba47670634d6976
Rachel <ral4123@g.rit.edu>
2023-09-26 13:03:22 -0400

    starting off basic

---
+++ system_report.py
@@ -0,0 +1,20 @@
+import datetime
+import socket
+
+def system_report():
+    print('\033[96mSystem Report\033[0m')
+    todays_date = datetime.datetime.now()
+    print("Today's Date:   " + todays_date.strftime("%Y-%m-%d"))
+    print("Current Time:   " + todays_date.strftime("%H:%M:%S"))
+    hostname = socket.gethostname()
+    print("Hostname:       " + hostname)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    print("IP Address:     " + s.getsockname()[0])
+
+
+def main():
+    system_report()
+
+if __name__ == "__main__":
+    main()
